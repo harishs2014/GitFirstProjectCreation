33f9ba813704281f3e5e714a49bdb8a04511e1a7
harishs2014 <39235147+harishs2014@users.noreply.github.com>
2019-03-19 23:04:09 +0530

    Add files via upload
    sadfdfd

---
+++ ExcelLibrary.java
@@ -0,0 +1,59 @@
+package com.qspiders.frameworkengine;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelLibrary {
+
+	/**
+	 * @param args
+	 */	
+	public String getExcelData(String sheetName, int rowNum, int cellNum){
+		String retVal=null;
+		try {
+			FileInputStream fis = new FileInputStream("D:\\testscenarios.xlsx");
+			Workbook wb = WorkbookFactory.create(fis);
+			Sheet s = wb.getSheet(sheetName);
+			Row r = s.getRow(rowNum);
+			Cell  c= r.getCell(cellNum);			
+			retVal = c.getStringCellValue();			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return retVal;
+	}
+
+	public int getRowCount(String sheetName){
+		int retVal=0;
+		try {
+			FileInputStream fis = new FileInputStream("D:\\testscenarios.xlsx");
+			Workbook wb = WorkbookFactory.create(fis);
+			Sheet s = wb.getSheet(sheetName);
+			retVal=s.getLastRowNum();			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return retVal;
+	}
+}
+
+
+
+
+
+
